734232b8b69932d94b36088bef8660909a8418d4
kcandeloni <kcandeloni@gmail.com>
2024-01-21 20:29:22 -0300

    lista de exercícios 1

---
+++ java-estudos/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bem vindo a lista de exercícios de Java!");
+        
+        ListExercicios praticar = new ListExercicios();
+        
+        praticar.oQueEhEsseNumero(8);
+    }
+}
---
+++ java-estudos/src/ListExercicios.java
@@ -0,0 +1,20 @@
+public class ListExercicios {
+
+    public void oQueEhEsseNumero(int n){
+
+        if(n % 2 == 0){
+            if(n >= 0){
+                System.err.println(n + " é par e positivo.");
+                return;
+            }
+            System.err.println(n + " é par e negativo.");
+        }else{
+            if(n >= 0){
+                System.err.println(n + " é impar e positivo.");
+                return;
+            }
+            System.err.println(n + " é impar e negativo.");
+        }
+    }
+    
+}
